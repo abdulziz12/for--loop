6b57463a1cc9f1729b16a57d05f53b73e6bc581b
abdulziz12 <abdurashidov200930@gmail.com>
2024-07-11 13:42:21 +0500

    eddassf
    dcdsfzcx

---
+++ script.js
@@ -0,0 +1,49 @@
+let berilganArray = [2, 4, 9, 10, 45, 67, 8, 90];
+let juftSonlar = [];
+for (let i = 0; i < berilganArray.length; i++) {
+    if (berilganArray[i] % 2 === 0) {
+        juftSonlar.push(berilganArray[i]);
+    }
+}
+
+
+console.log(berilganArray,'boshlanishidagi array')
+console.log(juftSonlar,'juft sonlargaajratilgan sonlar');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
